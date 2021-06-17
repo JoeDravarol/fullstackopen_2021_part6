5f2c722c6cb6eca87853b520316c3f56833fc045
Joe <joedravarol@gmail.com>
2021-06-17 13:44:58 +0100

    Complete anecdotes exercise step3 feat: ordered anecdotes by number of votes.

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
+  const orderedAnecdotes = useSelector(state => state.sort((a,b) => b.votes - a.votes))
   const dispatch = useDispatch()
 
   const vote = (id) => {
@@ -29,7 +29,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {orderedAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}

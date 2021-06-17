375031683198047a9657c086ff8beb645f83b789
Joe <joedravarol@gmail.com>
2021-06-17 14:02:30 +0100

    Complete anecdotes exercise step6 refactor: Seperate the rendering of the anecdote list into its own component.

---
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <div>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+const AnecdoteList = () => {
+  const orderedAnecdotes = useSelector(state => state.sort((a,b) => b.votes - a.votes))
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch( voteAnecdote(id) )
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {orderedAnecdotes.map(anecdote =>
+        <Anecdote 
+          key={anecdote.id}
+          anecdote={anecdote}
+          handleClick={() => vote(anecdote.id)}
+        />
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList

03768d43fb346d726e482f99ba6160b7bfeb8cf9
Joe <joedravarol@gmail.com>
2021-06-17 13:58:25 +0100

    refactor: Move header title into component

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -13,12 +13,15 @@ const AnecdoteForm = () => {
   }
 
   return (
-    <form onSubmit={addAnecdote}>
-      <div>
-        <input name='content' />
-      </div>
-      <button type='submit'>create</button>
-    </form>
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name='content' />
+        </div>
+        <button type='submit'>create</button>
+      </form>
+    </div>
   )
 }
 

eb57e21c5f90e458fcfa51326fdecfd2f26ba794
Joe <joedravarol@gmail.com>
2021-06-18 22:39:31 +0100

    Complete anecdotes and the backend exercise step4 refactor: Modify the creation of a new anecdote to happen using asynchronous action creators.

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification, resetNotification } from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
@@ -12,8 +11,7 @@ const AnecdoteForm = () => {
     const content = event.target.content.value
     event.target.content.value = ''
     
-    const newAnecdote = await anecdoteService.createNew(content)
-    dispatch( createAnecdote(newAnecdote) )
+    dispatch( createAnecdote(content) )
 
     dispatch( setNotification(`you created '${content}'`) )
     setTimeout(() => {
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -24,10 +24,13 @@ const reducer = (state = [], action) => {
   }
 }
 
-export const createAnecdote = (data) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    data
+export const createAnecdote = content => {
+  return async dispatch => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      data: newAnecdote,
+    })
   }
 }
 
@@ -45,7 +48,7 @@ export const initializeAnecdotes = () => {
     const anecdotes = await anecdoteService.getAll()
     dispatch({
       type: 'INIT_ANECDOTES',
-    data: anecdotes
+      data: anecdotes
     })
   }
 }

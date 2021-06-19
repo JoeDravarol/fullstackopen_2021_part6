651a21ab6a7f909d16a733db646f2c25e41ec0d9
Joe <joedravarol@gmail.com>
2021-06-19 12:37:58 +0100

    Complete anecdotes and connect exercise step1 refactor: Modify the Notification component to use connect function instead of hooks to access the redux store.

--- redux-anecdotes/src/components/Notification.js
+++ redux-anecdotes/src/components/Notification.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = ({ notification }) => {
   const style = {
     border: 'solid',
     padding: 10,
@@ -18,4 +17,10 @@ const Notification = () => {
   )
 }
 
-export default Notification
+const mapStateToProps = state => (
+  { notification: state.notification }
+)
+
+export default connect(
+  mapStateToProps
+)(Notification)
